a71a477b6e25845a3a6260b7c039c15196f6f92b
joshuarg007 <42895544+joshuarg007@users.noreply.github.com>
2021-03-04 23:22:26 -0700

    Added generic game loop

---
+++ RPG-Survival/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Invector.vCharacterController;
+
+public class WaveManager : MonoBehaviour
+{
+    private vThirdPersonController controller;
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<vThirdPersonController>();
+        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/AssetBundles/scenes");
+        scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controller.currentHealth <= 0)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
