0d2c4675ce71303fcee985491ce18f618ce16f94
joshuarg007 <42895544+joshuarg007@users.noreply.github.com>
2021-05-07 15:39:19 -0600

    Deleted everything except characters and dungeon

--- RPG-Survival/Assets/Invector-3rdPersonController/Melee Combat/Scripts/MeleeWeapon/vIAttackReceiver.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-namespace Invector.vEventSystems
-{
-    public interface vIAttackReceiver
-    {        
-        void OnReceiveAttack(vDamage damage, vIMeleeFighter attacker);
-    }
-}
-
